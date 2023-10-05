73bed24fa8da565c3866465379a2b4d53a50f552
Marcos Cozatti <marcoscozatti@gmail.com>
2023-10-04 22:06:24 -0300

    Projeto 1 - versão 2

--- WebApplication5/Controllers/HomeController.cs
+++ WebApplication5/Controllers/HomeController.cs
@@ -33,6 +33,12 @@ namespace WebApplication5.Controllers
             return View();
         }
 
+        public IActionResult pagTeste3()
+        {
+            return View();
+        }
+
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
